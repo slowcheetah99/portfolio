296e46320845273be66666a396b737d47d891fbd
slowcheetah99 <rgaronderdev@gmail.com>
2023-02-04 19:09:54 +0300

    fixed api connections

--- src/hooks/useServer.jsx
+++ src/hooks/useServer.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 const Instance = axios.create({
-  baseURL: import.meta.env.VITE_SERVERLESS_URL,
+  baseURL: "https://portfolio-serverless-func.vercel.app/",
   timeout: 5000,
   headers: {
     "Content-Type": "application/json",
